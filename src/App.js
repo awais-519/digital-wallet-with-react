7ef2236b819232b4145815cbdb8673b094ed0752
awais-519 <awaisul519@gmail.com>
2023-04-07 03:24:18 +0500

    Strip: Added Stripe.js and Elements

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 			<header className='App-header'>
 				<img src={logo} className='App-logo' alt='logo' />
 				<p>
-					Edit <code>src/App.js</code> and save to reload and see the magic.
+					Edit <code>src/App.js</code> and save to reload.
 				</p>
 				<a
 					className='App-link'
--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<Elements stripe={stripePromise}>
+			<App />
+		</Elements>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
